5732dd6e3a0457c511822b6c0be303acc0437e58
Dave Parr <8363743+DaveParr@users.noreply.github.com>
2023-11-13 19:34:24 +0000

    Minimally working RAG QA

--- starpilot/main.py
+++ starpilot/main.py
@@ -1,17 +1,23 @@
 import typer
+from typing_extensions import Optional
 from github import Github
 import starpilot.utils.utils as utils
 import dotenv
 import os
 from rich import print
+from langchain import hub
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.llms import GPT4All
 from langchain.embeddings import GPT4AllEmbeddings
 from langchain.vectorstores import Chroma
 import shutil
-from typing_extensions import Optional
 
 app = typer.Typer()
 
 dotenv.load_dotenv()
+
+VECTORSTORE_PATH = "./vectorstore-chroma"
 
 try:
     git_hub_key = os.getenv("GITHUB_API_KEY")
@@ -49,6 +55,8 @@ def read(
 
     repo_readmes = utils.get_repo_readmes(user_stars, GITHUB_CONNECTION)
 
+    print(f"Mapping {len(repo_readmes)} stars...")
+
     utils.save_readmes_to_disk(repo_readmes, user_stars)
 
     readme_splits = utils.prepare_github_readmes(path=readme_path)
@@ -57,8 +65,6 @@ def read(
 
     if os.path.exists(vectorstore_path):
         shutil.rmtree(vectorstore_path)
-
-    print("Mapping your stars...")
 
     Chroma.from_documents(
         documents=readme_splits,
@@ -75,17 +81,41 @@ def shoot(query: str):
     Shoot your stars to discover the most similar READMEs
     """
 
-    vectorstore_path = "./vectorstore-chroma"
-
-    if not os.path.exists(vectorstore_path):
+    if not os.path.exists(VECTORSTORE_PATH):
         raise Exception("Please load the stars before shooting")
 
     vectorstore_retrival = Chroma(
-        persist_directory="./vectorstore-chroma",
+        persist_directory=VECTORSTORE_PATH,
         embedding_function=GPT4AllEmbeddings(disallowed_special=()),
     )
-
     results = vectorstore_retrival.similarity_search(query)
 
     for result in results:
         print(result)
+
+
+@app.command()
+def horoscope(question: str):
+    retriever = Chroma(
+        persist_directory=VECTORSTORE_PATH,
+        embedding_function=GPT4AllEmbeddings(disallowed_special=()),
+    ).as_retriever(
+        search_type="mmr",
+        search_kwargs={"fetch_k": 3},
+    )
+    
+    rag_prompt = hub.pull("rlm/rag-prompt")
+
+    model_path = "./models/mistral-7b-openorca.Q4_0.gguf"  # replace with your desired local file path
+
+    # Callbacks support token-wise streaming
+    callbacks = [StreamingStdOutCallbackHandler()]
+
+    # Verbose is required to pass to the callback manager
+    llm = GPT4All(model=model_path, callbacks=callbacks, verbose=True)
+
+    rag_chain = (
+        {"context": retriever, "question": RunnablePassthrough()} | rag_prompt | llm
+    )
+
+    rag_chain.invoke(question)
--- starpilot/utils/utils.py
+++ starpilot/utils/utils.py
@@ -3,7 +3,7 @@ import os
 from langchain.document_loaders import UnstructuredMarkdownLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from github.Repository import Repository
-from github import Github
+from github import Github, UnknownObjectException
 from langchain.schema.document import Document
 
 
@@ -28,27 +28,14 @@ def get_repo_readmes(repos: List[Repository], g: Github) -> List[Repository]:
     for repo in repos:
         try:
             readme = repo.get_contents("README.md")
-            readmes.append(readme.decoded_content.decode())
-        except:
-            readmes.append(f"Repo: {repo.name} does not have a README.md file")
+        except UnknownObjectException:
+            try:
+                readme = repo.get_contents("README.rst")
+            except UnknownObjectException:
+                readme = None
+        if readme is not None:
+            readmes.append(readme.decoded_content.decode("utf-8"))
     return readmes
-
-
-def prepare_github_readmes(path: str) -> List[Document]:
-    file_paths = []
-    for file in os.listdir(path):
-        file_paths.append(os.path.join(path, file))
-
-    documents = []
-    for file_path in file_paths:
-        loader = UnstructuredMarkdownLoader(file_path)
-        documents.extend(loader.load())
-
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
-
-    splits = text_splitter.split_documents(documents)
-
-    return splits
 
 
 def save_readmes_to_disk(
@@ -57,7 +44,25 @@ def save_readmes_to_disk(
     if not os.path.exists(directory):
         os.makedirs(directory)
     for i in range(len(readmes)):
-        with open(
-            f"{directory}/{repos[i].name}.md", "w"
-        ) as f:  # FIXME: Should this be md?
+        with open(f"{directory}/{repos[i].name}.md", "w") as f:
+            # IDEA: Treat `RST` with more care
             f.write(readmes[i])
+
+
+def prepare_github_readmes(path: str) -> List[Document]:
+    # IDEA: This could be refactored into a DocumentLoader. If it is then it would read files sequentially, and then ditch them to keep only 1 in memory at once.
+    file_paths = []
+    for file in os.listdir(path):
+        file_paths.append(os.path.join(path, file))
+
+    documents = []
+    for file_path in file_paths:
+        loader = UnstructuredMarkdownLoader(file_path)
+        # IDEA: modify behaviour for RST vs MD: https://api.python.langchain.com/en/latest/document_loaders/langchain.document_loaders.rst.UnstructuredRSTLoader.html
+        documents.extend(loader.load())
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+
+    splits = text_splitter.split_documents(documents)
+
+    return splits
